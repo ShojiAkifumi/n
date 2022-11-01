bd6acece04754edaf896337e42e2a2601ed114f3
aki <kuonnrh48951@outlook.jp>
2022-11-01 11:08:56 +0900

    my first commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
+if (!process.env.WORDPRESS_API_URL) {
+    throw new Error(`
+    Please provide a valid WordPress instance URL.
+    Add to your environment variables WORDPRESS_API_URL.
+  `);
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        domains: [
+            "https://blogly.wp.xdomain.jp",
+        ],
+    },
 }
 
 module.exports = nextConfig
